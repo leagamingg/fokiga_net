85565ae969c98710dc17a15260a4b407e6763a98
吕嘉铭 <lvjiaming@xmfunny.com>
2026-03-17 12:45:38 +0800

    1.GameMode与GameState添加

---
+++ Source/fokigaServer/Private/MyGameMode.cpp
@@ -0,0 +1,73 @@
+﻿#include "MyGameMode.h"
+#include "MyServerGameState.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/PlayerState.h"
+
+AMyGameMode::AMyGameMode()
+{
+	GameStateClass = AMyServerGameState::StaticClass();
+}
+
+void AMyGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("GameMode BeginPlay"));
+}
+
+void AMyGameMode::StartPlay()
+{
+	Super::StartPlay();
+	AMyServerGameState* GS = GetGameState<AMyServerGameState>();
+	if (GS)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("GameState connected"));
+	}
+}
+
+void AMyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	if (!NewPlayer) return;
+
+	APlayerState* PS = NewPlayer->GetPlayerState<APlayerState>();
+	if (!PS) return;
+
+	// 分配ID
+	int32 AssignedId = NextPlayerId++;
+
+	// 记录
+	PlayerMap.Add(AssignedId, PS);
+	ReversePlayerMap.Add(PS, AssignedId);
+
+	// 你也可以把ID写进 PlayerState
+	PS->SetPlayerId(AssignedId);
+
+	UE_LOG(LogTemp, Warning, TEXT("Player Joined: ID=%d"), AssignedId);
+}
+
+void AMyGameMode::Logout(AController* Exiting)
+{
+	Super::Logout(Exiting);
+
+	if (!Exiting) return;
+
+	APlayerState* PS = Exiting->GetPlayerState<APlayerState>();
+	if (!PS) return;
+
+	if (ReversePlayerMap.Contains(PS))
+	{
+		int32 Id = ReversePlayerMap[PS];
+
+		PlayerMap.Remove(Id);
+		ReversePlayerMap.Remove(PS);
+
+		UE_LOG(LogTemp, Warning, TEXT("Player Left: ID=%d"), Id);
+	}
+}
+
+AActor* AMyGameMode::ChoosePlayerStart_Implementation(AController* Player)
+{
+	return Super::ChoosePlayerStart_Implementation(Player);
+}
---
+++ Source/fokigaServer/Public/MyGameMode.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MyGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOKIGASERVER_API AMyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+public:
+	AMyGameMode();
+
+protected:
+
+	virtual void StartPlay() override;
+	
+	// Match 流程
+	virtual void BeginPlay() override;
+
+	// 玩家登录
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+
+	// 玩家登出
+	virtual void Logout(AController* Exiting) override;
+
+	// 玩家重生点选择
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+
+private:
+	// 自增ID
+	int32 NextPlayerId = 1;
+
+	// ID -> PlayerState
+	UPROPERTY()
+	TMap<int32, APlayerState*> PlayerMap;
+
+	// 反查（方便删除）
+	UPROPERTY()
+	TMap<APlayerState*, int32> ReversePlayerMap;
+};
